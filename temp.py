20a2a81cdea0030df30d90b530e519ae62b9d746
faroukjnr <farouk60@gmail.com>
2022-04-11 10:13:26 +0000

    Added a for loop

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
 print("My name is christian")
 temp = input("What is your name? ")
-print(f"My name is Christian and your name is {temp})
+print(f"My name is Christian and your name is {temp}")
+for i in range(19):
+	print("This is spam")
